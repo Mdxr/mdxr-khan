55323fd4cdc61938cb7ac1b3ae30c5f7da9eb940
Mudasser Khan <83152394+Mdxr@users.noreply.github.com>
2024-05-07 17:19:43 +0500

    updated css & js
    added new cards styles, background & interactions

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 const hamIcon = document.getElementById("ham-icon");
 const hamMenu = document.getElementById("ham-menu");
 
+const selector = document.querySelector(".selector");
+const dOption = document.querySelector(".d-option");
+const sOptions = document.querySelectorAll(".s-option");
+const oOptions = document.querySelectorAll(".o-option");
+
+selector.addEventListener('click', () => {
+  selector.classList.toggle('active');
+})
+
+sOptions.forEach(option => {
+  option.addEventListener('click', () => {
+    dOption.textContent = option.textContent;
+  })
+})
+oOptions.forEach(option => {
+  option.addEventListener('click', () => {
+    dOption.textContent = option.id;
+  })
+})
+
 hamIcon.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
 });
@@ -11,8 +31,9 @@ function send() {
   let userName = document.getElementById("user-name").value;
   let userEmail = document.getElementById("user-email").value;
   let userMessage = document.getElementById("user-message").value;
+  let service = dOption.value;
   let body =
-    "<h1>" + userName + "</h1> <br/> " + userEmail + " <br/>" + userMessage;
+    "<h1>" + userName + "</h1> <br/> <span style='color:blue;'>" + userEmail + "</span> <br/>"+ service + "<br/>" + userMessage;
 
   if (userName === '' || userEmail === '' || userMessage === '') {
     return 
